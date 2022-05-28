3fc2d1e251980bfa4996c5370cf3e14870b084ab
Paqih Teguh Maulana <75491223+paqihteguh2324@users.noreply.github.com>
2022-05-28 11:40:09 +0700

    Delete coreui-free-react-admin-template-main/src directory

--- coreui-free-react-admin-template-main/src/api/index.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-const url = 'https://2c51-114-124-131-112.ap.ngrok.io';
-export default url;
-//export const createTodo = newTodo => axios.post(url, newTodo);
--- coreui-free-react-admin-template-main/src/views/buttons/index.js
+++
@@ -1,6 +0,0 @@
-import ButtonDropdowns from './ButtonDropdowns'
-import ButtonGroups from './ButtonGroups'
-import Buttons from './Buttons'
-import TambahData from './TambahData'
-
-export { ButtonDropdowns, ButtonGroups, Buttons, TambahData }
